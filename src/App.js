3c5b92497236c2b29a02fdd76eb72fd10ff844f6
Quanluis <quanluis@protonmail.com>
2019-04-10 16:57:10 -0400

    Implemented a simple Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import './app.css'
 
 class App extends Component {
   render() {
     return (
-     <div>
-       <h1>Hello</h1>
-     </div>
+  <div>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <a className="nav-item nav-link" href="#">Home</a>
+            <a className="nav-item nav-link" href="#">About</a>
+            <a className="nav-item nav-link" href="#">Contact</a>
+      </div>
+  </div>
+</nav>
+          </div>
+       <h1 className = "text-primary">Bootstrap Website</h1>
+  </div>
     );
   }
 }
